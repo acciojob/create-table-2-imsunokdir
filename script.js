21f8fbba4d2fedfd003acbbd5200ae459c903c2c
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-24 18:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+const table = document.getElementById("myTable");
+let startRow=0;
+
 function createTable() {
     //Write your code here
-  
+	let rn = Number(prompt("nput number of rows"));
+	let cn = Number(prompt("Input number of columns"));
+	let i=0;
+	for(i=startRow;i<rn+startRow;i++){
+		console.log(i)
+		let newTr = document.createElement("tr");
+		for(j=0;j<cn;j++){
+			let newTd = document.createElement("td");
+			newTd.textContent = `Row-${i} Column-${j}`
+			newTr.appendChild(newTd);
+		}
+		table.appendChild(newTr)
+	}
+	startRow=i;
+	
 }
